b4b42e676e5fd49e203059c9fc6046ddaefdaf12
Niharika Rastogi <dev.niharikarastogi@gmail.com>
2017-04-23 18:40:44 +0530

    feat: Add an empty activity for Projection of data

---
+++ app/src/main/java/com/emi_calculator/ProjectionActivity.java
@@ -0,0 +1,13 @@
+package com.emi_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projection);
+    }
+}
