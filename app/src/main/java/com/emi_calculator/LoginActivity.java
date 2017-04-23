4fb61e6532b5b82e71d3d533026100516d11d6cc
Niharika Rastogi <dev.niharikarastogi@gmail.com>
2017-04-23 17:37:05 +0530

    feat: Remove Bottom Navigation View for Simplicity

--- app/src/main/java/com/emi_calculator/LoginActivity.java
+++ app/src/main/java/com/emi_calculator/LoginActivity.java
@@ -1,13 +1,11 @@
 package com.emi_calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 
 import com.emi_calculator.Data.Query;
 
@@ -15,7 +13,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @BindView(R2.id.name)
     TextInputEditText name;
@@ -24,34 +22,11 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R2.id.mob_no)
     TextInputEditText mobile;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_login:
-
-                    return true;
-                case R.id.navigation_principal:
-
-                    return true;
-                case R.id.navigation_projection:
-
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        BottomNavigationView navigation = ButterKnife.findById(this, R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @OnClick(R2.id.submit)
@@ -76,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         query.EMAIL = email.getText().toString();
         query.save();
 
-        Log.d("MainActivity", "Data Saved!");
+        Log.d("LoginActivity", "Data Saved!");
+        startActivity(new Intent(this, PrincipalInputActivity.class));
     }
 }
