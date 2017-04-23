f9ac4f1784623d70a560847bbd191b1f548278f6
Niharika Rastogi <dev.niharikarastogi@gmail.com>
2017-04-23 16:36:42 +0530

    feat: Add a model "Query" for "Queries" database

---
+++ app/src/main/java/com/emi_calculator/Data/Query.java
@@ -0,0 +1,24 @@
+package com.emi_calculator.Data;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by Niharika Rastogi on 23-04-2017.
+ */
+@Table(name = "Queries", id = "_id")
+public class Query extends Model {
+
+    @Column
+    public String NAME;
+    @Column(unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
+    public String MOBILE_NUMBER;
+    @Column
+    public String QUERY;
+
+    Query() {
+        super();
+    }
+
+}
